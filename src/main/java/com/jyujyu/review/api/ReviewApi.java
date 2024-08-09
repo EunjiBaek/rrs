0bcc6c4e4d9823fec91a6af9d97a77502cdf6487
EunjiBaek <qordmswl5631@naver.com>
2024-08-09 18:28:24 +0900

    commit

--- src/main/java/com/jyujyu/review/api/ReviewApi.java
+++ src/main/java/com/jyujyu/review/api/ReviewApi.java
@@ -3,7 +3,9 @@ package com.jyujyu.review.api;
 
 import com.jyujyu.review.api.request.CreateReviewRequest;
 import com.jyujyu.review.service.ReviewService;
+import com.jyujyu.review.service.dto.ReviewDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +23,14 @@ public class ReviewApi {
     @DeleteMapping("/review/{reviewId}")
     public void deleteReview(@PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}/reviews")
+    public ReviewDto getRestaurantReviews(
+            @PathVariable("restaurantId") Long restaurantId,
+            @RequestParam("offset") Integer offset,
+            @RequestParam("limit") Integer limit
+    ) {
+        return reviewService.getRestaurantReviews(restaurantId, PageRequest.of(offset / limit, limit));
     }
 }
